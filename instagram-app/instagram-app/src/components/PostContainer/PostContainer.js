eb1d420cee729baaf8916631f02b53b2444264f2
HaBuDeSu <ruf.kenichi@gmail.com>
2019-05-27 12:20:21 -0500

    like button adds to likes

--- instagram-app/instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import './PostContainer.css';
 
-function PostContainer(props) {
-  return (
-    <div className="post-container">
-      <div className="post-header">
-        <img src={props.post.thumbnailUrl} alt={props.post.username}></img>
-        {props.post.username}
-      </div>
-      <div className="post-image">
-        <img src={props.post.imageUrl} alt={props.post.username}></img>
-      </div>
-      <div className="icon-row">
-        <div className="icons-left">
-          <img src="https://img.icons8.com/material-outlined/24/000000/like.png" className="like-button"></img>
-          <img src="https://img.icons8.com/ios/24/000000/comments.png" className="comment-button"></img>
-          <img src="https://img.icons8.com/material-outlined/24/000000/share-rounded.png" className="share-button"></img>
+class PostContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      likes: props.post.likes
+    }
+  }
+
+  addLike = e => {
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+
+  render () {
+    return (
+      <div className="post-container">
+        <div className="post-header">
+          <img src={this.props.post.thumbnailUrl} alt={this.props.post.username}></img>
+          {this.props.post.username}
         </div>
-        <div className="icons-right">
-          <img src="https://img.icons8.com/material-outlined/24/000000/bookmark-ribbon.png" className="bookmark-button"></img>
+        <div className="post-image">
+          <img src={this.props.post.imageUrl} alt={this.props.post.username}></img>
+        </div>
+        <div className="icon-row">
+          <div className="icons-left">
+            <img
+              src="https://img.icons8.com/material-outlined/24/000000/like.png"
+              className="like-button"
+              onClick={this.addLike}></img>
+            <img src="https://img.icons8.com/ios/24/000000/comments.png" className="comment-button"></img>
+            <img src="https://img.icons8.com/material-outlined/24/000000/share-rounded.png" className="share-button"></img>
+          </div>
+          <div className="icons-right">
+            <img src="https://img.icons8.com/material-outlined/24/000000/bookmark-ribbon.png" className="bookmark-button"></img>
+          </div>
+        </div>
+        <p className="likes">{this.state.likes} likes</p>
+        <div className="comment-section">
+          {this.props.commentsection}
         </div>
       </div>
-      <p className="likes">{props.post.likes} likes</p>
-      <div className="comment-section">
-        {props.commentsection}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default PostContainer;
