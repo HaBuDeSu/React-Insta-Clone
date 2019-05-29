7dd6113b0757f0d03932cf263994858d77a9ffe9
Ken Ruf <ruf.kenichi@gmail.com>
2019-05-28 22:06:57 -0500

    created WithAuthenticate component file

---
+++ instagram-app/instagram-app/src/authentication/withAuthenticate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class WithAuthenticate extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      
+    )
+  }
+}
+
+export default WithAuthenticate;
